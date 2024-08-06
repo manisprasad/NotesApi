8a6660e00e90dac042f81924807db379d30ca250
Manish Prasad <100564494+manisprasad@users.noreply.github.com>
2024-08-06 11:16:40 -0700

    cross orgin added

---
+++ AuthenticationController.java
@@ -0,0 +1,33 @@
+package org.manis.notes.controllers;
+
+import org.manis.notes.models.AuthenticationResponse;
+import org.manis.notes.models.User;
+import org.manis.notes.services.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody User request) {
+        AuthenticationResponse response = authenticationService.register(request);
+        if (response.getAccessToken() == null) {
+            return ResponseEntity.badRequest().body(response);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody User request) {
+        AuthenticationResponse response = authenticationService.authenticate(request);
+        return ResponseEntity.ok(response);
+    }
+}
