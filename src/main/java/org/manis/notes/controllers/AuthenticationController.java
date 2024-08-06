8de0068f15dc51f87150316f159df14932e4a409
Manish Prasad <100564494+manisprasad@users.noreply.github.com>
2024-08-06 11:50:48 -0700

    Add files via upload

--- src/main/java/org/manis/notes/controllers/AuthenticationController.java
+++ src/main/java/org/manis/notes/controllers/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5175/")
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
--- src/main/java/org/manis/notes/controllers/UserController.java
+++ src/main/java/org/manis/notes/controllers/UserController.java
@@ -8,16 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5175/")
 @RequestMapping("/user")
 public class UserController {
 
